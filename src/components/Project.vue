728c6b21a5a411178e178c2c411cbafa3ab49bb6
octojon <staplejm@dukes.jmu.edu>
2019-12-08 21:14:04 -0500

    Made home page

---
+++ src/components/Project.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="project">
+    <b-card
+      :title="title"
+      :sub-title="'by ' + author"
+      :img-src="img"
+      img-alt="project image"
+      img-top
+      tag="project"
+      style="max-width: 20rem;"
+      class="mb-2"
+    >
+      <b-card-text>
+        {{description}}
+      </b-card-text>
+
+      <b-button :href="link" variant="primary">Visit project</b-button>
+    </b-card>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      id: String,
+      title: String,
+      author: String,
+      description: String,
+      link: String,
+      img: String
+    },
+  }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <b-container class="mt-4 mb-4">
+      <h1>Girls Who Code at Harrisonburg High School</h1>
+      <p>Welcome to the Harrisonburg High School Girls Who Code project gallery! You can find projects by our club members below. Feel free to explore and share!</p>
+    </b-container>
+    <hr>
+    <b-container>
+      <Project v-for="post in posts" v-bind:key="post.id" v-bind="post" />
+    </b-container>
+
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Project from "@/components/Project.vue";
 
 export default {
-  name: "home",
   components: {
-    HelloWorld
+    Project
+  },
+  data() {
+    return {
+      posts: [
+        { 
+          id: "0",
+          title: "Click the Balloons!",
+          author: "Slaw Paw",
+          description: "This project was made with Scratch, a block-based coding language created by the MIT Media Lab.",
+          link: "https://scratch.mit.edu/projects/351891137/fullscreen/",
+          img: "https://picsum.photos/600/300/?image=25"
+        }
+      ]
+    }
   }
-};
+}
+
 </script>
